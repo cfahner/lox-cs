ff5b293b20124bbf053556c6b1455d9cced59e96
C. Fahner <github.cfahner.nough8@fahner.eu>
2025-11-24 20:23:20 +0100

    Adds AstPrinter, a very basic pretty printer

---
+++ lox-cs/Parser/Visitors/AstPrinter.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace Lox.Parser.Visitors
+{
+    public sealed class AstPrinter : IExprVisitor<string>
+    {
+        public string Print(Expr expr) => expr.Accept(this);
+
+        public string VisitBinary(Expr.Binary binary)
+            => Parenthesize(binary.Operator.Lexeme, binary.Left, binary.Right);
+
+        public string VisitGrouping(Expr.Grouping grouping)
+            => Parenthesize("group", grouping.Expression);
+
+        public string VisitLiteral(Expr.Literal literal)
+            => literal.Value.ToString() ?? "nil";
+
+        public string VisitUnary(Expr.Unary unary)
+            => Parenthesize(unary.Operator.Lexeme, unary.Right);
+
+        private string Parenthesize(string name, params Expr[] exprs)
+        {
+            return new StringBuilder($"({name}")
+                .Append(string.Concat(exprs.Select(expr => $" {expr.Accept(this)}")))
+                .Append(')')
+                .ToString();
+        }
+    }
+}
