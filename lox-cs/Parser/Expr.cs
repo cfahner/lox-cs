c36153de35a68037f549e75cc0b5e730a78261e5
C. Fahner <github.cfahner.nough8@fahner.eu>
2025-11-24 20:22:33 +0100

    Adds Expr and IExprVisitor

---
+++ lox-cs/Parser/Expr.cs
@@ -0,0 +1,29 @@
+﻿using Lox.Scanner;
+
+namespace Lox.Parser
+{
+    public abstract record Expr
+    {
+        public record Binary(Expr Left, Token Operator, Expr Right) : Expr
+        {
+            public override R Accept<R>(IExprVisitor<R> visitor) => visitor.VisitBinary(this);
+        }
+
+        public record Grouping(Expr Expression) : Expr
+        {
+            public override R Accept<R>(IExprVisitor<R> visitor) => visitor.VisitGrouping(this);
+        }
+
+        public record Literal(object Value) : Expr
+        {
+            public override R Accept<R>(IExprVisitor<R> visitor) => visitor.VisitLiteral(this);
+        }
+
+        public record Unary(Token Operator, Expr Right) : Expr
+        {
+            public override R Accept<R>(IExprVisitor<R> visitor) => visitor.VisitUnary(this);
+        }
+
+        public abstract R Accept<R>(IExprVisitor<R> visitor);
+    }
+}
---
+++ lox-cs/Parser/IExprVisitor.cs
@@ -0,0 +1,13 @@
+﻿namespace Lox.Parser
+{
+    public interface IExprVisitor<R>
+    {
+        R VisitBinary(Expr.Binary binary);
+
+        R VisitGrouping(Expr.Grouping grouping);
+
+        R VisitLiteral(Expr.Literal literal);
+
+        R VisitUnary(Expr.Unary unary);
+    }
+}
