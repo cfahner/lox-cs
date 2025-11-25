69de130f2392b8ae815dd9adbe67ddef7f897237
C. Fahner <github.cfahner.nough8@fahner.eu>
2025-11-25 18:31:28 +0100

    Adds ParseError

---
+++ lox-cs/Parser/ParseError.cs
@@ -0,0 +1,9 @@
+﻿using Lox.Scanner;
+
+namespace Lox.Parser
+{
+    public class ParseError(Token token, string message) : Exception(message)
+    {
+        public Token Token { get; } = token;
+    }
+}
