4d06a90b90a6d635b5d17f9478323038a07d3a81
C. Fahner <github.cfahner.nough8@fahner.eu>
2025-11-26 20:24:05 +0100

    Adds DivisionByZeroError

---
+++ lox-cs/Interpreter/RuntimeErrors/DivisionByZeroError.cs
@@ -0,0 +1,9 @@
+﻿using Lox.Scanner;
+
+namespace Lox.Interpreter.RuntimeErrors
+{
+    public class DivisionByZeroError(Token token) : RuntimeError(token, ErrorMessage)
+    {
+        private const string ErrorMessage = "Division by zero.";
+    }
+}
