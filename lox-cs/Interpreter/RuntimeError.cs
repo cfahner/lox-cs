177d7f9ca252b51bf4a91e53e43ac6440a36f398
C. Fahner <github.cfahner.nough8@fahner.eu>
2025-11-26 19:35:58 +0100

    Adds RuntimeError

---
+++ lox-cs/Interpreter/RuntimeError.cs
@@ -0,0 +1,9 @@
+﻿using Lox.Scanner;
+
+namespace Lox.Interpreter
+{
+    public class RuntimeError(Token token, string message) : Exception(message)
+    {
+        public Token Token { get; private init; } = token;
+    }
+}
