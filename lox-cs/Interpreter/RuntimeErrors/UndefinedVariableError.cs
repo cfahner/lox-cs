7d8e1f3431bef5f2cf6f551bce5cf1ebc966a2b1
C. Fahner <github.cfahner.nough8@fahner.eu>
2025-11-27 18:48:44 +0100

    Adds UndefinedVariableError

---
+++ lox-cs/Interpreter/RuntimeErrors/UndefinedVariableError.cs
@@ -0,0 +1,9 @@
+﻿using Lox.Scanner;
+
+namespace Lox.Interpreter.RuntimeErrors
+{
+    public class UndefinedVariableError(Token token)
+        : RuntimeError(token, $"Undefined variable '{token.Lexeme}'.")
+    {
+    }
+}
