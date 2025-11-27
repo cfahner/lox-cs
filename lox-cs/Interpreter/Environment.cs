b40f2931c2d909b8efd6c730ebaa296eaa17b75f
C. Fahner <github.cfahner.nough8@fahner.eu>
2025-11-27 19:43:17 +0100

    Adds Environment, for storing (global) variables

---
+++ lox-cs/Interpreter/Environment.cs
@@ -0,0 +1,22 @@
+﻿using Lox.Interpreter.RuntimeErrors;
+using Lox.Scanner;
+
+namespace Lox.Interpreter
+{
+    internal class Environment()
+    {
+        private readonly Dictionary<string, object?> _values = [];
+
+        public void Define(string name, object? value)
+        {
+            _values[name] = value;
+        }
+
+        public object? Get(Token name)
+        {
+            return _values.TryGetValue(name.Lexeme, out var value)
+                ? value
+                : throw new UndefinedVariableError(name);
+        }
+    }
+}
