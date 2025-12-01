6fd53f39f6efc7147ce31ddaf70db24c66f259d0
C. Fahner <github.cfahner.nough8@fahner.eu>
2025-12-01 19:38:09 +0100

    Add ArgumentCountError

---
+++ lox-cs/Interpreter/RuntimeErrors/ArgumentCountError.cs
@@ -0,0 +1,9 @@
+﻿using Lox.Scanner;
+
+namespace Lox.Interpreter.RuntimeErrors
+{
+    public class ArgumentCountError(Token token, int actualCount, int expectedCount)
+        : RuntimeError(token, $"Too few arguments passed to callable on line {token.Line}, {actualCount} passed and exactly {expectedCount} expected.")
+    {
+    }
+}
