373d515a008045c7f78b2224ed35197ef3824d0e
C. Fahner <github.cfahner.nough8@fahner.eu>
2025-12-01 19:39:50 +0100

    Add Clock : NativeLoxCallable, which represents the "clock()" function call

---
+++ lox-cs/Interpreter/Natives/Clock.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Lox.Interpreter.Natives
+{
+    internal class Clock : NativeLoxCallable
+    {
+        public override int Arity => 0;
+
+        public override object? Call(Interpreter interpreter, IEnumerable<object?> arguments)
+        {
+            return (double)(TimeProvider.System.GetLocalNow().Ticks / TimeSpan.TicksPerMillisecond);
+        }
+    }
+}
---
+++ lox-cs/Interpreter/Natives/NativeLoxCallable.cs
@@ -0,0 +1,11 @@
+﻿namespace Lox.Interpreter.Natives
+{
+    internal abstract class NativeLoxCallable : ILoxCallable
+    {
+        public abstract int Arity { get; }
+
+        public abstract object? Call(Interpreter interpreter, IEnumerable<object?> arguments);
+
+        public sealed override string ToString() => "<native fn>";
+    }
+}
