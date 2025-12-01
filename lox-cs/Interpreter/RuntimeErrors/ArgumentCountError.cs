5a26d8af04b0ae453163a85730dc7f4a9386beed
C. Fahner <github.cfahner.nough8@fahner.eu>
2025-12-01 20:37:44 +0100

    Update ArgumentCountError to improve the error message

--- lox-cs/Interpreter/RuntimeErrors/ArgumentCountError.cs
+++ lox-cs/Interpreter/RuntimeErrors/ArgumentCountError.cs
@@ -3,7 +3,7 @@
 namespace Lox.Interpreter.RuntimeErrors
 {
     public class ArgumentCountError(Token token, int actualCount, int expectedCount)
-        : RuntimeError(token, $"Too few arguments passed to callable on line {token.Line}, {actualCount} passed and exactly {expectedCount} expected.")
+        : RuntimeError(token, $"Wrong amount of arguments passed to callable on line {token.Line}, {actualCount} passed and exactly {expectedCount} expected.")
     {
     }
 }
