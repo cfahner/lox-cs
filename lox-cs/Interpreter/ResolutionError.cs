187dd168ead7abd3e9c2d227f8ff50d948a8cf0a
C. Fahner <github.cfahner.nough8@fahner.eu>
2025-12-05 21:00:53 +0100

    Add ResolutionError

---
+++ lox-cs/Interpreter/ResolutionError.cs
@@ -0,0 +1,9 @@
+﻿using Lox.Scanner;
+
+namespace Lox.Interpreter
+{
+    public class ResolutionError(Token token, string message) : Exception(message)
+    {
+        public Token Token { get; private init; } = token;
+    }
+}
