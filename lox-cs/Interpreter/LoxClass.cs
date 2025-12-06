c3d6d2a5f8bac2b9a0c5ad6d1782fa7118b0d832
C. Fahner <github.cfahner.nough8@fahner.eu>
2025-12-06 07:10:46 +0100

    Implement class instantiation

--- lox-cs/Interpreter/LoxClass.cs
+++ lox-cs/Interpreter/LoxClass.cs
@@ -1,9 +1,13 @@
 ﻿namespace Lox.Interpreter
 {
-    public class LoxClass(string name)
+    public class LoxClass(string name) : ILoxCallable
     {
         public string Name { get; private init; } = name;
 
+        public int Arity => 0;
+
         public override string ToString() => Name;
+
+        public object? Call(Interpreter interpreter, object?[] arguments) => new LoxInstance(this);
     }
 }
