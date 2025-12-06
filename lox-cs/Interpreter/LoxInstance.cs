828f8319f81e1cb050182d9ddf07180a325533d3
C. Fahner <github.cfahner.nough8@fahner.eu>
2025-12-06 07:10:36 +0100

    Add LoxInstance

---
+++ lox-cs/Interpreter/LoxInstance.cs
@@ -0,0 +1,9 @@
+﻿namespace Lox.Interpreter
+{
+    internal class LoxInstance(LoxClass @class)
+    {
+        private readonly LoxClass _class = @class;
+
+        public override string ToString() => $"<instance of {_class.Name}>";
+    }
+}
