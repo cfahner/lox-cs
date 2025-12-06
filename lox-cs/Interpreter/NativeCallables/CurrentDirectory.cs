47acb082619c0db263e7a29b0e2b09d7b01b511f
C. Fahner <github.cfahner.nough8@fahner.eu>
2025-12-06 06:30:08 +0100

    Add CurrentDirectory global function

---
+++ lox-cs/Interpreter/NativeCallables/CurrentDirectory.cs
@@ -0,0 +1,12 @@
+﻿namespace Lox.Interpreter.NativeCallables
+{
+    internal class CurrentDirectory : NativeLoxCallable
+    {
+        public override int Arity => 0;
+
+        public override object? Call(Interpreter interpreter, object?[] arguments)
+        {
+            return Directory.GetCurrentDirectory();
+        }
+    }
+}
