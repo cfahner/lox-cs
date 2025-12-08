551c7a60f0621ed1e050a562bc7dafcadf7d0ea7
C. Fahner <github.cfahner.nough8@fahner.eu>
2025-12-08 19:20:54 +0100

    Refactor LoxClass to support constructor function "init"

--- lox-cs/Interpreter/LoxClass.cs
+++ lox-cs/Interpreter/LoxClass.cs
@@ -4,13 +4,25 @@
     {
         public string Name { get; private init; } = name;
 
-        public int Arity => 0;
+        public int Arity => TryGetMethod("init", out var initializer)
+            ? initializer.Arity
+            : 0;
 
         private readonly IDictionary<string, LoxFunction> _methods = methods;
 
         public override string ToString() => Name;
 
-        public object? Call(Interpreter interpreter, object?[] arguments) => new LoxInstance(this);
+        public object? Call(Interpreter interpreter, object?[] arguments)
+        {
+            var instance = new LoxInstance(this);
+
+            if (TryGetMethod("init", out var initializer))
+            {
+                _ = initializer.Bind(instance).Call(interpreter, arguments);
+            }
+
+            return instance;
+        }
 
         public bool TryGetMethod(string name, out LoxFunction method)
         {
