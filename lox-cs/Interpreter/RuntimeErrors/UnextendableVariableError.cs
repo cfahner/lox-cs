8e1d62efe693103dcd7ab20cd15b45f102f15fac
C. Fahner <github.cfahner.nough8@fahner.eu>
2025-12-09 19:54:56 +0100

    Add UnextendableVariableError

---
+++ lox-cs/Interpreter/RuntimeErrors/UnextendableVariableError.cs
@@ -0,0 +1,9 @@
+﻿using Lox.Scanner;
+
+namespace Lox.Interpreter.RuntimeErrors
+{
+    public class UnextendableVariableError(Token token)
+        : RuntimeError(token, $"Cannot extend variable '{token.Lexeme}'.")
+    {
+    }
+}
