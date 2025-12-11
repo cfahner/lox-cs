9bd3d33edc6e8642b4fed9914f945d49dca65bfd
C. Fahner <github.cfahner.nough8@fahner.eu>
2025-12-11 20:28:05 +0100

    Add UndefinedPropertyError

---
+++ lox-cs/Interpreter/RuntimeErrors/UndefinedPropertyError.cs
@@ -0,0 +1,9 @@
+﻿using Lox.Scanner;
+
+namespace Lox.Interpreter.RuntimeErrors
+{
+    public class UndefinedPropertyError(Token property)
+        : RuntimeError(property, $"Undefined property '{property.Lexeme}'.")
+    {
+    }
+}
